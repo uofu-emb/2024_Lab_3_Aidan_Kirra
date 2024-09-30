47f1ade8834f6859802323ab1073392658eb1afc
akcase18 <aidan.case@casefamily.us>
2024-09-30 12:49:05 -0600

    Added functions to signaling.c

--- src/signaling.c
+++ src/signaling.c
@@ -1,1 +1,10 @@
 #include "signaling.h"
+
+void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
+{
+    
+}
+
+BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
+{
+}
