3a27cc665573b867f780516b9d4f1238d93ef72b
akcase18 <aidan.case@casefamily.us>
2024-10-02 13:00:58 -0600

    Added comments

--- src/fifo.c
+++ src/fifo.c
@@ -4,10 +4,10 @@ void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id)
 {
     while (1)
     {
-        struct request_msg msg;
-        xQueueReceive(requests, &msg, portMAX_DELAY);
-        msg.output = msg.input + 5;
-        msg.handled_by = id;
-        xQueueSendToBack(results, &msg, portMAX_DELAY);
+        struct request_msg msg;                         // Setup struct
+        xQueueReceive(requests, &msg, portMAX_DELAY);   // Get data from queue
+        msg.output = msg.input + 5;                     // Add 5 to input, save to output
+        msg.handled_by = id;                            // Save worker thread id
+        xQueueSendToBack(results, &msg, portMAX_DELAY); // Send data back
     }
 }
--- src/signaling.c
+++ src/signaling.c
@@ -3,26 +3,25 @@
 QueueHandle_t queue;
 
 /**
- * Takes in data from signal_request_calculate(), adds 5 to the input, 
+ * Takes in data from signal_request_calculate(), adds 5 to the input,
  * and sends modified data back.
  */
 void signal_handle_calculation(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
-    xSemaphoreTake(request, portMAX_DELAY);
-    data->output = data->input + 5;
-    vTaskDelay(10);
-    xSemaphoreGive(response);
+    xSemaphoreTake(request, portMAX_DELAY); // Wait for semaphore to stop blocking
+    data->output = data->input + 5;         // Add 5 to input, save to output
+    vTaskDelay(10);                         // Delay cuz idk why
+    xSemaphoreGive(response);               // Unblock semaphore to finish response
 }
 
 /**
  * This is the function that is getting called
  * This function will take in the semaphores and the data, and pass it off to
- * signal_handle_calculation(). 
+ * signal_handle_calculation().
  */
 BaseType_t signal_request_calculate(SemaphoreHandle_t request, SemaphoreHandle_t response, struct signal_data *data)
 {
-    int32_t input = data->input;
-    xSemaphoreGive(request);
-    BaseType_t result = xSemaphoreTake(response, 100);
+    xSemaphoreGive(request);                           // Unblock semaphore to finish request
+    BaseType_t result = xSemaphoreTake(response, 100); // Wait for other function to finish logic
     return result;
 }
